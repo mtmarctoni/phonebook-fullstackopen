d3f54e68f8630d013a4809b1f13da9d85ccebec4
mtmarctoni <marctonimas@protonmail.com>
2024-11-20 14:22:39 +0100

    uibuild

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
-const Person = require('./models/person')
+const Person = require('./models/Person')
 const mongoose = require('mongoose')
 
 // connect to mongoose
 const MONGO_DB_URI = process.env.MONGO_DB_URI
-console.log(MONGO_DB_URI);
+console.log(MONGO_DB_URI)
 
 mongoose.set('strictQuery',false)
 
-console.log('Connecting to MongoDB...');
+console.log('Connecting to MongoDB...')
 mongoose.connect(MONGO_DB_URI)
-    .then(res => {
-        console.log('Connected to MongoDB');
+    .then(() => {
+        console.log('Connected to MongoDB')
     })
     .catch(err => {
-        console.log('Error connecting to MongoDB:', err);
-})
+        console.log('Error connecting to MongoDB:', err)
+    })
 
 const PORT = process.env.PORT || 3001
 morgan.token('body', req =>
@@ -30,12 +30,22 @@ morgan.token('body', req =>
 const unknownEndpoint = (req, res) => {
     res.status(404).send({ error: 'unknown endpoint' })
 }
-    
+
 const errorHandler = (err, req, res, next) => {
     console.error(err.message)
     if (err.name === 'CastError') {
         return res.status(400).send({ error: 'malformatted id' })
+    } else if (err.name === 'ValidationError') {
+        return res.status(400).send({ error: err.message })
+    } else if (err.name === 'MongoError') {
+        return res.status(400).send({ error: 'Database error' })
+    } else if (err.name === 'TypeError') {
+        return res.status(400).send({ error: `TypeError: ${err.message}` })
     }
+
+
+
+
     next(err)
 }
 
@@ -45,20 +55,6 @@ app.use(cors())
 //app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-/*
-//Get all persons
-personsRouter.get("/api/persons", async (req, res) => {
-    const persons = await Person.find({})
-        .populate('user', {userName: 1, name: 1 })
-    
-      if (persons) {
-          res.json(persons)
-        } else {
-          res.satus(404).end()
-        }
-  
-  })
-*/
 app.get('/info', async (req, res) => {
     const count = await Person.countDocuments({})
 
@@ -78,20 +74,15 @@ app.get('/api/persons/:id', async (req, res, next) => {
 
     try {
         const person = await Person.findById(id)
-        
         person
-        ? res.json(person)
-        : res.status(404).send({ error: 'not found' })
-        
+            ? res.json(person)
+            : res.status(404).send({ error: 'not found' })
+
     } catch (err) {next(err)}
 })
 
 app.delete('/api/persons/:id', async (req, res, next) => {
     const { id } = req.params
-    console.log('EN DELETE', id);
-    console.log('tipo id', typeof(id));
-    
-    
     try {
         const person = await Person.findByIdAndDelete(id)
         if (person) {
@@ -100,42 +91,41 @@ app.delete('/api/persons/:id', async (req, res, next) => {
             res.status(404).end()
         }
     } catch (err) {next(err)}
-        
 })
 
-app.post('/api/persons', async (req, res) => {
+app.post('/api/persons', async (req, res, next) => {
     const { name, number } = req.body
-    
+
     if (!name || !number) {
         return res.status(400).json({ error: 'content missing' })
     }
-    
-    //if (contacts.find(c => c.name === name)) {
-    //    return res.status(400).json({ error: 'name must be unique' })
-    //}
-    
+
     const newPerson = new Person ({
-        //id: generateId(),
         name: name,
         number: number
     })
 
-    const newPersonSaved = await newPerson.save()    
-    res.json(newPersonSaved)
-    
+    try {
+        const newPersonSaved = await newPerson.save()
+        res.json(newPersonSaved)
+    } catch (err) {next(err)}
+
 })
 
 app.put('/api/persons/:id', async (req, res, next) => {
     const { name, number } = req.body
-    const { id } = req.params
-    
+    //const { id } = req.params
+
     if (!name || !number) {
         return res.status(400).json({ error: 'content missing' })
     }
+
     try {
         const personToUpdate = await Person.find({ name: name })
         if (personToUpdate.length > 0) {
-            const updatedPerson = await Person.findByIdAndUpdate(personToUpdate[0].id, { number: number })
+            const updatedPerson = await Person.findByIdAndUpdate(personToUpdate[0].id,
+                { number: number },
+                { new: true, runValidators: true, context: 'query' })
             res.json(updatedPerson)
         } else {
             res.status(404).end()
@@ -146,7 +136,7 @@ app.put('/api/persons/:id', async (req, res, next) => {
 
 app.listen(PORT, () => {
     console.log('Listening on port', PORT)
-    
+
 })
 
 app.use(unknownEndpoint)
---
+++ models/Person.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const personSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 3
+    },
+    number: {
+        type: String,
+        required: true,
+        validate: {
+            validator: (v) => {
+                return /^(\d{2,3})-(\d+)$/.test(v) && v.length >= 8
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        }
+    },
+})
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Person', personSchema)
